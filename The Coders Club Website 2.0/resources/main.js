36cacb9e1ec22db0cd03457aa8551ae0eb726762
tech-world-code <64862958+tech-world-code@users.noreply.github.com>
2023-02-22 16:32:56 -0800

    Add files via upload
    Added redesigned nav bar. Correct grammar. Added virtual option for classes.

--- The Coders Club Website 2.0/resources/main.js
+++ The Coders Club Website 2.0/resources/main.js
@@ -1,48 +1,41 @@
-var exitButton = document.getElementById ("exit-button");
-        
+const exitButton = document.getElementById("exit-button");
 
-var exitButtonOnClick = function () {
-    mobileMenu.classList.add ("hidden");
+
+const exitButtonOnClick = () => {
+    mobileMenu.classList.add("hidden");
 }
-var mobileMenuButton = document.getElementById("mobile-menu-enter");
-var mobileMenu = document.getElementById("mobile-menu-id");
-var mainContent = document.getElementById("main-content")
-var footer = document.getElementsByTagName("footer")
+const mobileMenuButton = document.getElementById("mobile-menu-enter");
+const mobileMenu = document.getElementById("mobile-menu-id");
+const mainContent = document.getElementById("main-content")
+const footer = document.getElementsByTagName("footer")
 
 
-var mobileMenuButtonOnClick = function() {
+const mobileMenuButtonOnClick = () => {
 
-mainContent.classList.toggle("moved-away")
-if (mobileMenu.classList.contains("hidden") === true) {
-    footer[0].classList.toggle("hidden")
-setTimeout(function () {
-    mobileMenu.classList.remove("hidden"); 
-    
-}, 2000);
-}
-else {
-    mobileMenu.classList.add("hidden"); 
-    setTimeout(function () {
-        footer[0].classList.remove("hidden")
-    
-    }, 2000);
-    
-}
+    mainContent.classList.toggle("moved-away")
+    if (mobileMenu.classList.contains("hidden") === true) {
+        footer[0].classList.toggle("hidden")
+        setTimeout(() =>{
+            mobileMenu.classList.remove("hidden");
 
+        }, 2000);
+    }
+    else {
+        mobileMenu.classList.add("hidden");
+        setTimeout(() => {
+            footer[0].classList.remove("hidden")
 
+        }, 2000);
 
-if (mobileMenu.classList.contains("hidden")) {
-mobileMenuButton.innerHTML = "<div class='line'></div><br><div class='line'></div><br><div class='line'></div>";
-}
-
+    }
 
 
 };
 
 mobileMenuButton.addEventListener("click", mobileMenuButtonOnClick);
 
-var menuOption = document.getElementsByClassName("nav-option")
-var pagePath = window.location.pathname;
+const menuOption = document.getElementsByClassName("nav-option")
+const pagePath = window.location.pathname;
 
 if (pagePath === "/about.html") {
     menuOption[0].className = "nav-on-page nav-option";
@@ -55,11 +48,11 @@ else if (pagePath === "/contact.html") {
 }
 
 let height = screen.height;
-if ( !(pagePath == "/" || pagePath == "/index.html" || pagePath == "" )) { 
-document.querySelector(".welcome").style.minHeight=height+"px";
+if (!(pagePath == "/" || pagePath == "/index.html" || pagePath == "")) {
+    document.querySelector(".welcome").style.minHeight = height + "px";
 }
 
-document.querySelector(".mobile-menu").style.minHeight=height+"px";
+document.querySelector(".mobile-menu").style.minHeight = height + "px";
 
 console.log(pagePath)
 
